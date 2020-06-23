14ec2fbc4d8e621d1d53867d4b9686e75bd53cb9
Donald Leatherwood <doleathe@cisco.com>
2020-06-23 18:05:47 -0400

    Update Install-PowershellGitGet.ps1

--- Install-PowershellGitGet.ps1
+++ Install-PowershellGitGet.ps1
@@ -2,40 +2,28 @@
 
 $moduleName = "PowershellGitGet"
 $gitURL = "https://github.com/leeleatherwood/PowershellGitGet.git"
-
 $archiveURL = $GitURL.Replace(".git","/archive/master.zip")
 $outfilePath = $env:TEMP + "\" + $gitURL.Split("/")[-1] + ".zip"
 $outfolderPath = $outfilePath.Replace(".zip","")
 
-Write-Output "Downloading PowershellGitGet Archive"
 Invoke-WebRequest -Uri $archiveURL -OutFile $outfilePath -Headers @{"Cache-Control"="no-cache"}
-
-Write-Output "Expanding PowershellGitGet Archive"
 Remove-Item $outfolderPath -Recurse -Force -ErrorAction "SilentlyContinue"
 Expand-Archive -LiteralPath $outfilePath -DestinationPath $outfolderPath
 
-Write-Output "Loading PowershellGitGet psd data"
 $psdFile = Get-ChildItem –Path $outfolderPath -Include "*.psd1" -Recurse
 $psdData = Import-PowerShellDataFile -LiteralPath $psdFile.FullName
 $psModulePath = ([Environment]::GetEnvironmentVariable("PSModulePath", "Machine")).Split(";")[-1]
 $moduleDestination = "$psModulePath\$moduleName\" + $psdData.ModuleVersion
 
-Write-Output "Installing PowershellGitGet v$($psdData.ModuleVersion)"
 Remove-Item $moduleDestination -Recurse -Force -ErrorAction "SilentlyContinue"
 New-Item -Path $moduleDestination -ItemType "Directory" -Force | out-null
 Copy-Item -Path "$($psdFile.DirectoryName)\*" -Destination $moduleDestination -Recurse -Force
-$psdFile = Get-ChildItem –Path $moduleDestination -Include "*.psd1" -Recurse
-$psdData = Import-PowerShellDataFile -LiteralPath $psdFile.FullName
-
-Write-Output "Importing PowershellGitGet v$($psdData.ModuleVersion)"
-Import-Module $psdFile.FullName -Force -Verbose
-
-Write-Output "Removing temporary installation files"
+Import-Module (Get-ChildItem –Path $moduleDestination -Include "*.psd1" -Recurse).FullName -Force -Verbose
 Remove-Item $outfolderPath -Recurse -Force -ErrorAction "SilentlyContinue"
 
 if (Get-Module -Name 'PowershellGitGet')
 {
-	Write-Output "Installation Complete"
+	Write-Output "Installation of PowershellGitGet v$($psdData.ModuleVersion) Complete"
 }
 else
 {
