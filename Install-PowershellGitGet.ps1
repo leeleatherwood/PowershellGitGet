9ea5ef0875a667d170d304837178018a235c1f23
Donald Leatherwood <doleathe@cisco.com>
2020-06-23 17:26:14 -0400

    Update Install-PowershellGitGet.ps1

--- Install-PowershellGitGet.ps1
+++ Install-PowershellGitGet.ps1
@@ -1,8 +1,10 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$GitURL = 'https://github.com/leeleatherwood/PowershellGitGet.git'
+$moduleName = 'PowershellGitGet'
+$gitURL = 'https://github.com/leeleatherwood/PowershellGitGet.git'
+
 $archiveURL = $GitURL.Replace('.git',"/archive/master.zip")
-$outfilePath = $env:TEMP + '\' + $GitURL.Split('/')[-1] + ".zip"
+$outfilePath = $env:TEMP + '\' + $gitURL.Split('/')[-1] + ".zip"
 $outfolderPath = $outfilePath.Replace('.zip','')
 
 Invoke-WebRequest -Uri $archiveURL -OutFile $outfilePath
@@ -13,7 +15,7 @@ Remove-Item $outfilePath -Force -ErrorAction 'SilentlyContinue'
 $psdFile = Get-ChildItem –Path $outfolderPath -Include "*.psd1" -Recurse -ErrorAction 'SilentlyContinue'
 $psdData = Import-PowerShellDataFile -LiteralPath $psdFile.FullName
 $psModulePath = ([Environment]::GetEnvironmentVariable("PSModulePath", "Machine")).Split(';')[-1]
-$moduleDestination = "$psModulePath\$ModuleName\" + $psdData.ModuleVersion
+$moduleDestination = "$psModulePath\$moduleName\" + $psdData.ModuleVersion
 
 Remove-Item $moduleDestination -Recurse -Force -ErrorAction 'SilentlyContinue'
 New-Item -Path $moduleDestination -ItemType 'Directory' -Force | out-null
