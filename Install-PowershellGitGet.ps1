e6318252c631ec1e4bca03381af36c7ba4db333d
Donald Leatherwood <doleathe@cisco.com>
2020-06-23 17:33:54 -0400

    Update Install-PowershellGitGet.ps1

--- Install-PowershellGitGet.ps1
+++ Install-PowershellGitGet.ps1
@@ -1,22 +1,21 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$moduleName = 'PowershellGitGet'
-$gitURL = 'https://github.com/leeleatherwood/PowershellGitGet.git'
+$moduleName = "PowershellGitGet"
+$gitURL = "https://github.com/leeleatherwood/PowershellGitGet.git"
 
-$archiveURL = $GitURL.Replace('.git',"/archive/master.zip")
-$outfilePath = $env:TEMP + '\' + $gitURL.Split('/')[-1] + ".zip"
-$outfolderPath = $outfilePath.Replace('.zip','')
+$archiveURL = $GitURL.Replace(".git","/archive/master.zip")
+$outfilePath = $env:TEMP + "\" + $gitURL.Split("/")[-1] + ".zip"
+$outfolderPath = $outfilePath.Replace(".zip","")
 
-Invoke-WebRequest -Uri $archiveURL -OutFile $outfilePath
-Remove-Item $outfolderPath -Recurse -Force -ErrorAction 'SilentlyContinue'
+Invoke-WebRequest -Uri $archiveURL -OutFile $outfilePath -Headers @{"Cache-Control"="no-cache"}
+Remove-Item $outfolderPath -Recurse -Force -ErrorAction "SilentlyContinue"
 Expand-Archive -LiteralPath $outfilePath -DestinationPath $outfolderPath
-Remove-Item $outfilePath -Force -ErrorAction 'SilentlyContinue'
 
-$psdFile = Get-ChildItem –Path $outfolderPath -Include "*.psd1" -Recurse -ErrorAction 'SilentlyContinue'
+$psdFile = Get-ChildItem –Path $outfolderPath -Include "*.psd1" -Recurse
 $psdData = Import-PowerShellDataFile -LiteralPath $psdFile.FullName
-$psModulePath = ([Environment]::GetEnvironmentVariable("PSModulePath", "Machine")).Split(';')[-1]
+$psModulePath = ([Environment]::GetEnvironmentVariable("PSModulePath", "Machine")).Split(";")[-1]
 $moduleDestination = "$psModulePath\$moduleName\" + $psdData.ModuleVersion
 
-Remove-Item $moduleDestination -Recurse -Force -ErrorAction 'SilentlyContinue'
-New-Item -Path $moduleDestination -ItemType 'Directory' -Force | out-null
+Remove-Item $moduleDestination -Recurse -Force -ErrorAction "SilentlyContinue"
+New-Item -Path $moduleDestination -ItemType "Directory" -Force | out-null
 Copy-Item -Path "$($psdFile.DirectoryName)\*" -Destination $moduleDestination -Recurse -Force
